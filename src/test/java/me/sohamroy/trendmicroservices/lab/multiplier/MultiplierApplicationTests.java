2c2c95e2cab8328eb1ba5189a8e29a70be0c3450
Soham Roy <sohamroy@sohamroy.me>
2020-05-09 16:24:32 -0500

    Init commit

---
+++ src/test/java/me/sohamroy/trendmicroservices/lab/multiplier/MultiplierApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sohamroy.trendmicroservices.lab.multiplier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
